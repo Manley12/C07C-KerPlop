29d1c0936bf6108e3b1ff77481f458ab64d68bdd
CatMusty <57682141+CatMusty@users.noreply.github.com>
2021-02-04 11:24:21 -0600

    Add files via upload
    Initial

---
+++ C07C-KerPlop/src/levelPieces/LevelSetup.java
@@ -0,0 +1,28 @@
+package levelPieces;
+
+import java.util.ArrayList;
+import gameEngine.Drawable;
+import gameEngine.Moveable;
+
+public class LevelSetup {
+	public Drawable[] getBoard() {
+		return null;
+	}
+
+public void createLevel(int levelNum) {
+
+}
+
+public ArrayList<Moveable> getMovingPieces(){
+	return null;
+}
+
+public ArrayList<GamePiece> getInteractingPieces(){
+	return null;
+}
+
+public int getPlayerStartloc() {
+	return 1;
+}
+
+}
